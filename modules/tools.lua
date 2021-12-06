6c81ea16cb86f7a371c363611ec93533994b0fab
B0lu <boluelbot@gmail.com>
2021-12-06 02:47:36 +0100

    Warn on lua split() if separator is more than 1 char

--- modules/tools.lua
+++ modules/tools.lua
@@ -28,12 +28,35 @@ function luatools.readlines(file)
 end
 
 function luatools.split(str, sep)
+	if #sep > 1 then error("Split function can only take one-character separators") end
 	if sep == nil then sep = "%s" end
 	local r = {}
 	for s in string.gmatch(str, "([^" .. sep .. "]+)") do
 		table.insert(r, s)
 	end
 	return r
+end
+
+--[[ 
+-- Better string splitter (Norman Ramsey, https://stackoverflow.com/questions/1426954/split-string-in-lua)
+function string:split(pat)
+  pat = pat or '%s+'
+  local st, g = 1, self:gmatch("()("..pat..")")
+  local function getter(segs, seps, sep, cap1, ...)
+    st = sep and seps + #sep
+    return self:sub(segs, (seps or 0) - 1), cap1 or sep, ...
+  end
+  return function() if st then return getter(st, g()) end end
+end
+--]]
+
+local string_meta = getmetatable('')
+
+function string_meta:__index(key)
+	local val = string[key]	-- String functions are also accessed by "__index" (e.g: sub function).
+	if val then return val end -- These two lines are so lua still finds the builtin functions
+	if tonumber(key) then return self:sub(key, key) end
+	error("Cannot access index '" .. tostring(key) .. "'' of the string.")
 end
 
 return luatools
