2790bece831ae984a6cd614d2fb6c52b2ccdfa2d
B0lu <boluelbot@gmail.com>
2021-12-27 19:45:36 +0100

    Day 12

---
+++ 12/one.py
@@ -0,0 +1,40 @@
+# --- Advent of code 2021: Day 12 ---
+
+# (Skeleton file automatically created by aocTool, developed by Bolu, 2020-2021.)
+
+caves = open("input.txt").read().splitlines()
+
+connections = { cave.split("-")[0]: set() for cave in caves }
+for a, b in map(lambda cave: cave.split("-"), caves):
+	connections[a].add(b)
+
+small_caves = { cave.split("-")[1] for cave in caves if cave.split("-")[1][0] >= 'a' }
+for cave in caves:
+	if cave.split("-")[0][0] >= 'a':
+		small_caves.add(cave.split("-")[0])
+small_caves.remove('end')
+small_caves.remove('start')
+
+for a in list(connections.keys()):
+	for b in connections[a]:
+		if b == 'end':
+			continue
+		elif b not in connections:
+			connections[b] = set()
+		if a != 'start':
+			connections[b].add(a)
+
+def explore(node, caves, small_caves, explored, path, paths):
+	for nxt in caves[node]:
+		if nxt == 'end':
+			paths.append(path + ',end')
+		elif nxt not in explored:
+			explored_nxt = explored.copy()
+			if nxt in small_caves:
+				explored_nxt.add(nxt)
+			explore(nxt, caves, small_caves, explored_nxt, path + f",{nxt}", paths)
+
+paths = []
+explore('start', connections, small_caves, set(), 'start', paths)
+print(f"Amount of paths that only visit small caves at least once: {len(paths)}")
+assert len(paths) == 4104
---
+++ 12/two.lua
@@ -0,0 +1,58 @@
+-- --- Advent of code 2021: Day 12 ---
+
+-- (Skeleton file automatically created by aocTool, developed by Bolu, 2020-2021.)
+
+package.path = package.path .. ";../modules/?.lua"
+local tools = require "tools"
+
+caves = tools.readlines("input.txt")
+
+connections = {}
+small_caves = {}
+for _, cave in pairs(caves) do
+	org, dst = table.unpack(tools.split(cave, "-"))
+	if not connections[org] then
+		connections[org] = {}
+	end
+	connections[org][dst] = true
+	if dst ~= "end" and org ~= "start" then
+		if not connections[dst] then
+			connections[dst] = {}
+		end
+		connections[dst][org] = true
+	end
+	if org:sub(1, 1) >= 'a' then
+		small_caves[org] = true
+	end
+end
+small_caves["end"] = nil
+small_caves.start = nil
+
+function explore(node, caves, small_caves, explored, path, paths, twice)
+	twice = twice or false
+	for nxt, _ in pairs(caves[node]) do
+		if nxt == "end" then
+			paths[#paths + 1] = path .. ",end"
+		elseif not explored[nxt] then
+			explored_nxt = {}
+			for cave, _ in pairs(explored) do
+				explored_nxt[cave] = true
+			end
+			if small_caves[nxt] then
+				explored_nxt[nxt] = true
+			end
+			explore(nxt, caves, small_caves, explored_nxt, path .. "," .. tostring(nxt), paths, twice)
+		elseif not twice then
+			explored_nxt = {}
+			for cave, _ in pairs(explored) do
+				explored_nxt[cave] = true
+			end
+			explore(nxt, caves, small_caves, explored_nxt, path .. "," .. tostring(nxt), paths, true)
+		end
+	end
+end
+
+paths = {}
+explore("start", connections, small_caves, {}, "start", paths)
+print("Amount of paths that only visit small caves at least once and maybe one twice: " .. #paths)
+assert(#paths == 119760)
