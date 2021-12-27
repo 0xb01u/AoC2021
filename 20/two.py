92df00758913d1c50b7e2ce24041edfba2c5895d
B0lu <boluelbot@gmail.com>
2021-12-27 19:47:12 +0100

    Day 20

---
+++ 20/two.py
@@ -0,0 +1,53 @@
+# --- Advent of code 2021: Day 20 ---
+
+# (Skeleton file automatically created by aocTool, developed by Bolu, 2020-2021.)
+
+algorithm, image = open("input.txt").read().split("\n\n")
+image = image.splitlines()
+
+lit_pixels = {(i, j) for i in range(len(image)) for j in range(len(image[i])) if image[i][j] == '#'}
+
+def i2o(image, black=False):
+	global algorithm
+
+	_x = min({e[0] for e in image})
+	_y = min({e[1] for e in image})
+	x = max({e[0] for e in image}) + 1
+	y = max({e[1] for e in image}) + 1
+
+	out = set()
+	for i in range(_x - 2, x + 2):
+		for j in range(_y - 2, y + 2):
+			binnum = 0
+			for di in range(-1, 2):
+				for dj in range(-1, 2):
+					binnum <<= 1
+					if (i + di, j + dj) in image:
+						binnum |= 1
+					elif ((i + di) < _x or (i + di) >= x or (j + dj) < _y or (j + dj) >= y) and (algorithm[0] == '#' and not black):
+						binnum |= 1						
+
+			if algorithm[binnum] == '#':
+				out.add((i, j))
+
+	'''
+	for i in range(_x - 2, x + 2):
+		for j in range(_y - 2, y + 2):
+			if (i, j) in image:
+				print("#", end="")
+			else:
+				print(".", end="")
+		print()
+	print()
+	'''
+
+	return out
+
+res = lit_pixels
+black = True
+for _ in range(50):
+	res = i2o(res, black)
+	black = not black
+
+print(f"Lit pixels in the resulting image: {len(res)}")
+assert len(res) == 14052
